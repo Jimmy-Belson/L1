bd21b59165bef6e94f704fdcad717b1dd397caaf
Jimmy-Belson <farid.abbaszade2006@gmail.com>
2026-03-31 17:36:25 +0400

    Update script.js

--- script.js
+++ script.js
@@ -21,8 +21,12 @@ if (window.Core.sb) {
 
 const Core = {
     // Инициализируем клиент один раз при обращении
+    sb: (window.supabase) ? window.supabase.createClient(
+        'https://ebjsxlympwocluxgmwcu.supabase.co', 
+        'sb_publishable_8HhPj3Y8g5V7Np8Vy5xbzQ_2B7LjTkj'
+    ) : null,
 
-
+    user: null,
     
     toggleChat() {
         const chatWindow = document.getElementById('main-chat-window');
