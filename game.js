1258f566a49694b6020c4b6f9bf99c56841d823d
Jimmy-Belson <farid.abbaszade2006@gmail.com>
2026-04-01 16:53:33 +0400

    Update game.js

--- game.js
+++ game.js
@@ -3,7 +3,7 @@
 // ==========================================
 import { getRankByScore } from './ranks.js';
 
-const canvas = document.getElementById('starfield');
+const canvas = document.getElementById('game-canvas');
 const ctx = canvas.getContext('2d');
 
 // --- ГЛОБАЛЬНЫЙ КОНФИГ ---
